04537f5d94bcb90d6a336155250a95e56b881dae
Glen1986 <escobar.glen@gmail.com>
2022-05-03 22:20:33 -0300

    rendimiento-memo1

---
+++ src/components/Lists/MyList.js
@@ -0,0 +1,23 @@
+import { memo } from 'react'
+
+const Li = ({ fullname }) => {
+    console.log(`renderizando ${fullname}`)
+    return <li>{fullname}</li>
+}
+const MLi = memo(Li)
+
+const MyList = ({ data }) => {
+    console.log('renderizando lista')
+    return (
+        <ul>
+            {data.map((x) => (
+                <MLi
+                    key={x.name + x.lastname}
+                    fullname={`${x.name} ${x.lastname}`}
+                />
+            ))}
+        </ul>
+    )
+}
+
+export default MyList
